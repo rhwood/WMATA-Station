701c3fb05115bae7529732c201c08bd9c69b6eb5
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-01-03 20:55:10 -0500

    Initial Commit

---
+++ WMATA Station/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WMATA Station
+//
+//  Created by Randall Wood on 2021-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WMATA Station/WMATA_StationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMATA_StationApp.swift
+//  WMATA Station
+//
+//  Created by Randall Wood on 2021-01-03.
+//
+
+import SwiftUI
+
+@main
+struct WMATA_StationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
