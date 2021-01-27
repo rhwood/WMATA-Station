449df203fe0639dfa7bab8b22475f16783622986
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-01-27 07:00:42 -0500

    refactor: split WMATAUI into its own package
    This is a prepatory step to splitting WMATAUI into its own repo.

--- WMATA Station/Stores/LinesStore.swift
+++ WMATA Station/Stores/LinesStore.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import WMATA
+import WMATAUI
 
 class LinesStore: ObservableObject {
 
--- WMATA Station/Views/StationView.swift
+++ WMATA Station/Views/StationView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import WMATA
+import WMATAUI
 
 struct StationView: View {
 
