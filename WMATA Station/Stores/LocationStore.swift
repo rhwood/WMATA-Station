953aa6943806568091dfac8556afa7405d050dab
Randall Wood <297232+rhwood@users.noreply.github.com>
2021-08-30 07:33:16 -0400

    chore: flesh out framework for requesting location

--- WMATA Station/Stores/LocationStore.swift
+++ WMATA Station/Stores/LocationStore.swift
@@ -8,13 +8,16 @@
 import Foundation
 import CoreLocation
 import WMATA
+import os
 
 /// Store the current location and determine shortest walking distance to any given station
 public class LocationStore: NSObject, ObservableObject, CLLocationManagerDelegate {
 
     @Published var authorizationStatus: CLAuthorizationStatus
     @Published var closestStations: [Station]
-
+    @Published var location: CLLocation?
+    
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "", category: "LocationStore")
     private let locationManager: CLLocationManager
 
     override public init() {
@@ -26,8 +29,9 @@ public class LocationStore: NSObject, ObservableObject, CLLocationManagerDelegat
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         #if !os(tvOS)
-            locationManager.startUpdatingLocation()
+        locationManager.startUpdatingLocation()
         #endif
+        locationManagerDidChangeAuthorization(locationManager)
     }
 
     func requestPermission() {
@@ -36,6 +40,21 @@ public class LocationStore: NSObject, ObservableObject, CLLocationManagerDelegat
 
     public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         authorizationStatus = manager.authorizationStatus
+        if authorizationStatus == .authorizedWhenInUse {
+            manager.requestLocation()
+        }
     }
 
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last
+        getClosestStations()
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        logger.info("LocationManager failed: \(error.localizedDescription)")
+    }
+
+    func getClosestStations() {
+        logger.debug("Getting Closest Stations to \(self.location?.description ?? "[no location]")")
+    }
 }
